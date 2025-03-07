70b61b10448f9bb0d497a2a0a87615681316da1a
Pclinfo <developer.pclinfotech@gmail.com>
2025-03-07 17:24:57 +0530

    Update settings.py

--- API_e-commerce/config/settings.py
+++ API_e-commerce/config/settings.py
@@ -2,7 +2,7 @@ class Config:
     SECRET_KEY = "pclinfo"
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024
     UPLOAD_FOLDER='./uploads'
-    DB_NAME = "pclmart"
+    DB_NAME = "PCL"
     DB_USER = "postgres"
     DB_PASSWORD = "9514"
     DB_HOST = "147.79.66.39"
